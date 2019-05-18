8c91b349cddcceb7b6039e370e7f95a07c777c78
skaetch <godwinkachi@hotmail.com>
2019-05-18 20:48:59 +0100

    Completed the JS clock, and Updated the logo png

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var pullDisplay = document.getElementById('request').addEventListener('click', s
 
 function show(){
     document.getElementById('request-dropdown').style.display = 'block';
+    document.getElementById('githeader').textContent = 'GIT PR HISTORY';
     document.getElementById('issue-dropdown').style.display = 'none';
 }
 
@@ -11,6 +12,7 @@ addEventListener('click', hide);
 function hide(){
     document.getElementById('issue-dropdown').style.display = 'block';
     document.getElementById('request-dropdown').style.display ='none';
+    document.getElementById('githeader').textContent = 'GIT ISSUES HISTORY';
 }
 
 
@@ -20,4 +22,26 @@ function hide(){
 //     document.getElementById('scrollable').style.overflow = 'scroll';
 //     document.getElementById('scrollable').style.overflow='auto';
 // }
+var months = ['January', 'February','March','April','May','June','July','August','September','October','November','December'];
+var now = new Date();
+var thisMonth = months[now.getMonth()];
+document.getElementById('month').innerHTML = thisMonth;
 
+var today = now.getDate();
+document.getElementById('day').innerHTML = today;
+
+var year = now.getFullYear();
+document.getElementById('year').innerHTML = year;
+
+var hourCalc = now.getHours();
+var hour = now.getHours()%12;
+document.getElementById('hour').innerHTML = hour;
+
+var minute = now.getMinutes();
+document.getElementById('minute').innerHTML = minute;
+
+if (hourCalc >= 12) {
+    document.getElementById('session').innerHTML = "pm";
+}else{
+    document.getElementById('session').innerHTML = 'am';
+}
